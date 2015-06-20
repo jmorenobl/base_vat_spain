1d74fff475c35b43a9c0511a8ddddaf80f13313c
Jorge Moreno Blázquez <jorge.moreno@mixin.es>
2015-06-20 17:01:06 +0200

    Extend check_vat to allow vat others than starting with country code

--- base_vat.py
+++ base_vat.py
@@ -29,5 +29,32 @@ _logger = logging.getLogger(__name__)
 class res_partner(osv.osv):
     _inherit = 'res.partner'
 
+    def _isEU(self, vat):
+        if not vat[0:2].encode('utf-8').isalpha():
+            return False
+
+    def check_vat(self, cr, uid, ids, context=None):
+        user_company = self.pool.get('res.users').browse(cr, uid, uid).company_id
+        if user_company.vat_check_vies:
+            # force full VIES online check
+            check_func = self.vies_vat_check
+        else:
+            # quick and partial off-line checksum validation
+            check_func = self.simple_vat_check
+        for partner in self.browse(cr, uid, ids, context=context):
+            if not partner.vat:
+                continue
+
+            if self._isEU(patner.vat):
+                vat_country, vat_number = self._split_vat(partner.vat)
+            else:
+                vat_country = 'es'  # HACK: this should be get from company info. Can anyone help?
+                vat_number = partner.vat
+
+            if not check_func(cr, uid, vat_country, vat_number, context=context):
+                _logger.info(_("Importing VAT Number [%s] is not valid !" % vat_number))
+                return False
+        return True
+
     def check_vat_es(self, vat):
         return True
